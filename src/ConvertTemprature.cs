a67aac3a1ee1b6c6f38d5138b19fcea266e08da2
Abhishek Singh <singh90abhishek@gmail.com>
2023-10-19 16:09:21 +0300

    code changes has done

---
+++ src/ConvertTemprature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter.src
+{
+    static class ConvertTemprature
+    {
+        public static string TempConvert(string temp)
+        {
+            string [] str = temp.Split(" ");
+            var value = Convert.ToDouble (str[0]);
+            var unit = str[1];
+            var convertValue = "";
+
+            switch (unit)
+            {
+                case "C":
+                    convertValue = $"{Math.Round((value * 9) / 5 + 32,2)} F";
+                    break;
+                case "F":
+                    convertValue = $"{Math.Round((value - 32) * 5 / 9,2)} C"; 
+                    break;
+
+                default:
+                    Console.WriteLine("wrong input value");
+                    break;
+            }
+
+            return convertValue;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using TemperatureConverter.src;
+
+var input = "";
+
+Console.WriteLine(" \r\t Convert Temprature  ");
+Console.WriteLine("1. Enter valid temprature vlaue in format '{degree} {C|F}'.");
+Console.WriteLine("2. Enete exit to stop the applcation");
+do
+{
+    var pattern = @"^-?\.?\d+\.?\d*\s[CF]$";
+    Console.WriteLine("Please enter a temperature value and its unit of measurement (F or C):");
+    input = Console.ReadLine();
+    if (input == "exit")
+    {
+        break;
+    }
+    
+    var match = Regex.IsMatch(input??"", pattern);
+    if (match)
+    {
+        var result = ConvertTemprature.TempConvert(input ?? "");
+        Console.WriteLine($"{input} = {result}");
+    }
+    else {
+        Console.Write("Invalid input. ");
+    }
+    
+} while (true);
+Console.WriteLine("Program terminated");
+Environment.Exit(0);
+
+
